3ec47931492005dc5f14e871704a6e10871ffff0
Artur Sgibnev <55087938+Artur-Sg@users.noreply.github.com>
2022-07-13 20:14:24 +0400

    Fetch posts from API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useRef } from 'react';
 // import ClassCounter from './components/ClassCounter';
 // import Counter from './components/Counter';
 import './styles/app.css';
 import PostList from './components/PostList';
-// import AppButton from './components/UI/button/AppButton';
-// import AppInput from './components/UI/input/AppInput';
 import PostForm from './components/PostForm';
 import { PostFilter } from './components/PostFilter';
 import AppModal from './components/UI/modal/AppModal';
 import AppButton from './components/UI/button/AppButton';
 import { usePosts } from './hooks/usePosts';
-import { defaultPosts } from './DS/posts';
+import PostService from './API/PostService';
+import AppLoader from './components/UI/loader/AppLoader';
 
 function App() {
-  const [posts, setPosts] = useState(defaultPosts);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const sortedAndSearchedPosts = usePosts(posts, filter);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  async function fetchPosts() {
+    setIsLoading(true);
+
+    const posts = await PostService.getAll();
+    setPosts(posts);
+
+    setIsLoading(false);
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -42,11 +56,15 @@ function App() {
 
       <PostFilter filter={filter} setFilter={setFilter} />
 
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Posts List"
-      />
+      {isLoading ? (
+        <AppLoader />
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Posts List"
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/UI/loader/AppLoader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './AppLoader.module.css';
+
+const AppLoader = () => {
+  return <div className={classes.loader}></div>;
+};
+
+export default AppLoader;
